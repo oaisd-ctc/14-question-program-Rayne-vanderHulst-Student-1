908e1cb47428cf710cb5f51219a42c2d82b63959
Rayne <rava438@student.oaisd.org>
2024-10-14 08:20:01 -0400

    14 questions
    14 questions

---
+++ 14 questions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Program 
+{
+
+public static void Main(string[] args)    
+    {
+
+    Console.WriteLine("hello world!");
+    Console.WriteLine("my name is rayne vanderhulst");
+
+    //Using Variables
+    int age = 17;
+    string greeting = "hello world";
+    string firstname = "rayne";
+    string lastname = "vanderhulst";
+    double money = 2.79;
+
+
+    Console.WriteLine(greeting + "! " + "my name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on a Energy drink.");
+    }
+
+}
+
